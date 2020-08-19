9a8ddef79b8b9b1530a20b6abf5e619590c65e9c
larapollehn <lara.pollehn@googlemail.com>
2020-08-19 21:09:53 +0200

    linted the whole project

--- src/log/Logger.js
+++ src/log/Logger.js
@@ -1,13 +1,13 @@
-const log = require('loglevel');
-const prefix = require('loglevel-plugin-prefix');
-const chalk = require('chalk');
+const log = require("loglevel");
+const prefix = require("loglevel-plugin-prefix");
+const chalk = require("chalk");
 
 const colors = {
     TRACE: chalk.hex("#0022ff"),
     DEBUG: chalk.hex("#19ff00"),
     INFO: chalk.hex("#ffffff"),
     WARN: chalk.hex("#ff8100"),
-    ERROR: chalk.hex("#ff0000"),
+    ERROR: chalk.hex("#ff0000")
 };
 
 prefix.reg(log);
@@ -15,13 +15,13 @@ prefix.reg(log);
 prefix.apply(log, {
     format(level, name, timestamp) {
         return `${chalk.gray(`[${timestamp}]`)} ${colors[level.toUpperCase()](level)} ${chalk.green(`${name}:`)}`;
-    },
+    }
 });
 
-prefix.apply(log.getLogger('critical'), {
+prefix.apply(log.getLogger("critical"), {
     format(level, name, timestamp) {
         return chalk.red.bold(`[${timestamp}] ${level} ${name}:`);
-    },
+    }
 });
 
 log.setLevel(process.env.LOG_LEVEL || "info");
