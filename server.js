7da84b2a26c38f221c60610045b175742a0dce2e
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-20 18:22:56 +0200

    COmpression middleware

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require("dotenv-flow").config();
 const log = require("./src/log/Logger");
 const rateLimit = require("express-rate-limit");
 const helmet = require("helmet");
-const compression = require("compression")
+const compression = require("compression");
+const shouldCompress = require("./src/middleware/CompressionMiddleware");
 
 /**
  * Security configuration
@@ -15,16 +16,6 @@ const limiter = rateLimit({
 app.use(limiter);
 app.use(helmet());
 app.use(compression({ filter: shouldCompress }))
-
-function shouldCompress (req, res) {
-    if (req.headers["x-no-compression"]) {
-        // Don't compress responses with this request header
-        return false
-    }
-
-    // Fallback to standard filter function
-    return compression.filter(req, res)
-}
 
 
 /**
---
+++ src/middleware/CompressionMiddleware.js
@@ -0,0 +1,10 @@
+const compression = require("compression")
+
+const shouldCompress = (req, res) => {
+    if (req.headers["x-no-compression"]) {
+        return false
+    }
+    return compression.filter(req, res)
+}
+
+module.exports = shouldCompress;
