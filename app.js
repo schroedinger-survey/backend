0d4616fe141d31b407c3db79611cab694ae1fa36
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-20 20:49:41 +0200

    Swagger API

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./docs/swagger.json');
 const app = express();
 
 const userRouter = require("./src/router/UserRouters");
@@ -6,6 +8,8 @@ const userRouter = require("./src/router/UserRouters");
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
+
+app.use('/api', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/user", userRouter);
 
 module.exports = app;
