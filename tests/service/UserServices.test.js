70aef623300d63cae24f57d03b4c4de04adcd842
larapollehn <lara.pollehn@googlemail.com>
2020-08-20 15:47:52 +0200

    Many tests for the service user login

--- tests/service/UserServices.test.js
+++ tests/service/UserServices.test.js
@@ -14,7 +14,10 @@ describe("Basic tests for the API", () => {
     });
 
     test("The API for user registration should work", async (done) => {
-        const result = await request.post("/api/user").send({"username": "a1", "password": "b1", "email": "c1"});
+        const result = await request.post("/api/user").send({
+            "username": "a1",
+            "password": "b1",
+            "email": "c1"});
         expect(result.status).toBe(201);
 
         const duplicateUsername = await request.post("/api/user").send({
@@ -52,8 +55,33 @@ describe("Basic tests for the API", () => {
         done();
     });
 
+    test("The API for user login should work", async() => {
+        const registerUser = await request.post("/api/user").send({
+           "username": "test",
+           "password": "testpassword",
+           "email": "testmail"
+        });
+        expect(registerUser.status).toBe(201);
+
+        const loginUser = await request.post("/api/user/login").send({
+            "username": "test",
+            "password": "testpassword"
+        });
+        const jwtParts = loginUser.body.jwt.split(".");
+        const tokenPayload = await JSON.parse(atob(jwtParts[1]));
+        expect(loginUser.body.hasOwnProperty("jwt")).toBe(true);
+        expect(jwtParts.length).toBe(3);
+        expect(tokenPayload.hasOwnProperty("id")).toBe(true);
+        expect(tokenPayload.hasOwnProperty("username")).toBe(true);
+        expect(tokenPayload.hasOwnProperty("exp")).toBe(true);
+        expect(tokenPayload.hasOwnProperty("iat")).toBe(true);
+        expect(tokenPayload.username).toBe("test");
+        expect(tokenPayload.exp > tokenPayload.iat).toBe(true);
+        expect(loginUser.status).toBe(200);
+    })
+
     afterEach(async (done) => {
-        await sqlAccess.query("DELETE FROM users");
+        await sqlAccess.clearDatabase();
         done();
     });
 
