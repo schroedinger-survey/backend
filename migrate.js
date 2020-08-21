deeeaaa02b45c9652ebf92fa55010108aa86b522
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-21 08:44:05 +0200

    Advanced script migration

--- migrate.js
+++ migrate.js
@@ -2,12 +2,42 @@ require("dotenv-flow").config();
 const fs = require("fs").promises;
 const log = require("./src/log/Logger");
 const sqlAccess = require("./src/dataaccess/SQLAccess");
+const path = require('path');
 
 async function initialize() {
-    const data = await fs.readFile("scripts/001_create_tables.sql", "utf-8");
+    const files = await fs.readdir('scripts');
+    files.sort();
     try {
         await sqlAccess.begin();
-        await sqlAccess.query(data)
+        await sqlAccess.query(`
+            CREATE TABLE IF NOT EXISTS migration_scripts
+            (
+                id                    SERIAL PRIMARY KEY,
+                migration_script_name varchar(255) NOT NULL UNIQUE,
+                created               DATE         NOT NULL DEFAULT CURRENT_DATE
+            );
+        `);
+
+        for (let i = 0; i < files.length; i++) {
+            const filePath = path.join("scripts", files[i]);
+            const searchMigrationScript = {
+                name: "search-migration-script",
+                text: "SELECT * FROM migration_scripts WHERE migration_script_name =$1",
+                values: [filePath]
+            };
+            const checkScriptExists = await sqlAccess.query(searchMigrationScript);
+            if(checkScriptExists.rowCount === 0) {
+                const data = await fs.readFile(filePath, "utf-8");
+                await sqlAccess.query(data);
+
+                const insertMigrationScript = {
+                    name: "insert-migration-script",
+                    text: "INSERT INTO migration_scripts(migration_script_name) values($1)",
+                    values: [filePath]
+                };
+                await sqlAccess.query(insertMigrationScript);
+            }
+        }
         await sqlAccess.commit();
     } catch (e) {
         await sqlAccess.rollback();
@@ -17,10 +47,10 @@ async function initialize() {
     }
 }
 
-initialize().then(function (){
+initialize().then(function () {
     log.debug("Migrating database successfully.");
     process.exit(0);
-}).catch(function(e){
+}).catch(function (e) {
     log.error(e);
     process.exit(1);
 });
