721f4461bb3548220522131170e0f3d7de4209d8
Long Nguyen <long.nguyen2306@protonmail.com>
2020-08-21 10:55:41 +0200

    redis quit, not sql quite

--- tests/dataaccess/RedisAccess.test.js
+++ tests/dataaccess/RedisAccess.test.js
@@ -4,13 +4,12 @@ require("dotenv-flow").config({
 const {test, expect} = require("@jest/globals");
 const redisAccess = require("../../src/dataaccess/RedisAccess");
 const {v4: uuidv4} = require("uuid");
-const sqlAccess = require("../../src/dataaccess/SQLAccess");
 
 test("Test the redis connection", async (done) => {
     const key = uuidv4();
     const value = uuidv4();
     await redisAccess.set(key, value);
     expect(await redisAccess.get(key)).toEqual(value);
-    await sqlAccess.close();
+    await redisAccess.close();
     done();
 });
