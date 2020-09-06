c0fbc9c59bdcb8806a9c4e0d695e888aca0919c9
long nguyen <long.nguyen2306@protonmail.com>
2020-09-07 00:57:11 +0200

    migrate

--- migrate.js
+++ migrate.js
@@ -2,7 +2,7 @@ require("dotenv-flow").config();
 const fs = require("fs").promises;
 const postgresDB = require("./src/db/PostgresDB");
 const path = require("path");
-const Logger = require("src/utils/Logger");
+const Logger = require("./src/utils/Logger");
 
 const log = Logger("Migrate");
 
