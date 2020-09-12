6b60bb89dcb71d22d80175a0e367d8269eefeee1
Long Nguyen <long.nguyen2306@protonmail.com>
2020-09-12 10:42:08 +0200

    search survey

--- src/db/sql/SurveyDB.ts
+++ src/db/sql/SurveyDB.ts
@@ -59,9 +59,12 @@ class SurveyDB extends AbstractSqlDB {
                                                                                  'position', cq.position,
                                                                                  'options', (SELECT json_agg(
                                                                                                             json_build_object(
-                                                                                                                    'id', cqo.id,
-                                                                                                                    'answer', cqo.answer,
-                                                                                                                    'position', cqo.position
+                                                                                                                    'id',
+                                                                                                                    cqo.id,
+                                                                                                                    'answer',
+                                                                                                                    cqo.answer,
+                                                                                                                    'position',
+                                                                                                                    cqo.position
                                                                                                                 )
                                                                                                         )
                                                                                              FROM constrained_questions_options cqo
@@ -94,7 +97,7 @@ class SurveyDB extends AbstractSqlDB {
         return json;
     }
 
-    searchPublicSurveys = (user_id, title, description, startDate, endDate, pageNumber, pageSize) => {
+    searchSurveys = (user_id, title, description, secured, startDate, endDate, pageNumber, pageSize) => {
         let user_id_formatted = user_id;
         if (user_id_formatted) {
             user_id_formatted = user_id_formatted.split("-").join("");
@@ -107,15 +110,55 @@ class SurveyDB extends AbstractSqlDB {
         if (description_formatted) {
             description_formatted = `%${description_formatted}%`;
         }
-        return this.query(`WITH args (user_id, title, description, start_date, end_date) as (VALUES ($1, $2, $3, CAST($4 as Date), CAST($5 as Date)))
-            SELECT surveys.id FROM surveys, args
-            WHERE (surveys.secured is false)
-            AND (args.user_id IS NULL OR surveys.user_id = args.user_id::uuid) 
-            AND (args.title IS NULL OR surveys.title LIKE args.title) 
-            AND (args.start_date IS NULL OR surveys.start_date > args.start_date)
-            AND (args.end_date IS NULL OR surveys.end_date < args.start_date) 
-            ORDER BY surveys.created DESC OFFSET $6  LIMIT $7;`,
-            [user_id_formatted, title_formatted, description_formatted, startDate, endDate, pageNumber * pageSize, pageSize]
+        return this.query(`
+                            WITH args (user_id, title, description, secured, start_date, end_date) as (VALUES ($1, $2, $3, $4, CAST($5 as Date), CAST($6 as Date)))
+                            SELECT json_build_object(
+                                'id', s.id,
+                                'title', s.title,
+                                'user_id', s.user_id,
+                                'description', s.description,
+                                'start_date', s.start_date,
+                                'end_date', s.end_date,
+                                'secured', s.secured,
+                                'constrained_questions', (SELECT json_agg(
+                                                                         json_build_object(
+                                                                                 'id', cq.id,
+                                                                                 'question_text', cq.question_text,
+                                                                                 'position', cq.position,
+                                                                                 'options', (SELECT json_agg(
+                                                                                                            json_build_object(
+                                                                                                                    'id', cqo.id,
+                                                                                                                    'answer', cqo.answer,
+                                                                                                                    'position', cqo.position
+                                                                                                                )
+                                                                                                        )
+                                                                                             FROM constrained_questions_options cqo
+                                                                                             WHERE cq.id = cqo.constrained_question_id
+                                                                                 )
+                                                                             )
+                                                                     )
+                                                          FROM constrained_questions cq
+                                                          WHERE cq.survey_id = s.id),
+                                'freestyle_questions', (SELECT json_agg(
+                                                                       json_build_object(
+                                                                               'id', fq.id,
+                                                                               'question_text', fq.question_text,
+                                                                               'position', fq.position
+                                                                           )
+                                                                   )
+                                                        FROM freestyle_questions fq
+                                                        WHERE fq.survey_id = s.id
+                                )
+                            ) AS result
+                             FROM surveys s, args
+                             (args.secured IS NULL OR surveys.secured = args.secured::boolean)
+                             AND (args.user_id IS NULL OR surveys.user_id = args.user_id::uuid) 
+                             AND (args.title IS NULL OR surveys.title LIKE args.title) 
+                             AND (args.start_date IS NULL OR surveys.start_date > args.start_date)
+                             AND (args.end_date IS NULL OR surveys.end_date < args.start_date) 
+                             GROUP BY s.id
+                             ORDER BY surveys.created DESC OFFSET $6 LIMIT $7`,
+            [user_id_formatted, title_formatted, description_formatted, secured, startDate, endDate, pageNumber * pageSize, pageSize]
         );
     }
 
