24e4a89a1cdf518d173ba3992304cd517ce516af
Long Nguyen <long.nguyen2306@protonmail.com>
2020-09-30 10:39:14 +0200

    Cronjob should work

---
+++ schroedinger-cron.ts
@@ -0,0 +1,21 @@
+require("dotenv-flow").config({
+    silent: true
+});
+import elasticsearchDB from "./src/drivers/ElasticsearchDB";
+const CronJob = require('cron').CronJob;
+import loggerFactory from "./src/utils/Logger";
+
+const log = loggerFactory.buildDebugLogger("schroedinger-cron.ts");
+
+
+/**
+ * Cleaning Elasticsearch
+ * GET _cat/indices?v
+ */
+new CronJob('0 0 * * * *', async () => {
+    log.info("Start rotating elasticsearch logs")
+    const date = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
+    await elasticsearchDB.indices.delete({index: `metricbeat-7.9.1-${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}*`});
+    await elasticsearchDB.indices.delete({index: `.monitoring-es-7-${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}*`});
+    await elasticsearchDB.indices.delete({index: `.monitoring-kibana-7-${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}*`});
+}).start();
