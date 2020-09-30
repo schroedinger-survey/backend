5d6021307b9bc24d2d0726423e1f9511b061c10e
Long Nguyen <long.nguyen2306@protonmail.com>
2020-09-30 08:45:00 +0000

    Update schroedinger-cron.ts

--- schroedinger-cron.ts
+++ schroedinger-cron.ts
@@ -2,7 +2,7 @@ require("dotenv-flow").config({
     silent: true
 });
 import elasticsearchDB from "./src/drivers/ElasticsearchDB";
-const CronJob = require('cron').CronJob;
+const CronJob = require("cron").CronJob;
 import loggerFactory from "./src/utils/Logger";
 
 const log = loggerFactory.buildDebugLogger("schroedinger-cron.ts");
@@ -12,8 +12,8 @@ const log = loggerFactory.buildDebugLogger("schroedinger-cron.ts");
  * Cleaning Elasticsearch
  * GET _cat/indices?v
  */
-new CronJob('0 0 * * * *', async () => {
-    log.info("Start rotating elasticsearch logs")
+new CronJob("0 0 * * * *", async () => {
+    log.info("Start rotating elasticsearch logs");
     const date = new Date(Date.now() - 30 * 24 * 60 * 60 * 1000);
     await elasticsearchDB.indices.delete({index: `metricbeat-7.9.1-${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}*`});
     await elasticsearchDB.indices.delete({index: `.monitoring-es-7-${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}*`});
